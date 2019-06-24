bc632fce08ce47a0cd0ad170aa8f169717bb024c
Allison French <alliefrench22@gmail.com>
2019-06-24 12:30:15 -0400

    resolved client render bug

--- webpack.config.js
+++ webpack.config.js
@@ -8,7 +8,7 @@ module.exports = {
   ],
   output: {
     path: __dirname, // assumes your bundle.js will also be in the root of your project folder
-    filename: 'bundle.js',
+    filename: './public/bundle.js',
   },
   devtool: 'source-maps',
   module: {
