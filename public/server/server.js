f0bad26f5d551af83239351c15156c57fad6bb13
Allison French <alliefrench22@gmail.com>
2019-06-24 10:16:24 -0400

    creating server files

---
+++ public/server/server.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const app = express();
+const morgan = require('morgan');
+const bodyParser = require('body-parser');
+
+app.use(morgan('dev'));
+
+app.use(express.static(path.join(__dirname, '../public')));
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.use('/api', require('./api'));
+
+app.get('*', function(req, res) {
+  res.sendFile(path.join(__dirname, '../public'));
+});
+
+app.use(function(err, req, res, next) {
+  console.error(err);
+  console.error(err.stack);
+  res.status(err.status || 500).send(err.message || 'Internal server error');
+});
